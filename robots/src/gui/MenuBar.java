246c2b8d9dc3889fd9f55a5e91db8d2caacd28bb
mariamarfitcyna <marfitcynamaria@gmail.com>
2023-03-22 23:31:04 +0500

    menuBar class modifying has been started

---
+++ robots/src/gui/MenuBar.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MenuBar {
+
+    private JMenuBar menuBar;
+
+    public MenuBar(){
+
+        menuBar = new JMenuBar();
+    }
+
+    public JMenu addMenu(){
+
+    }
+
+    private JMenu createMenu(){
+
+    }
+
+}
