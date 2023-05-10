72ecae5f854a32a162250afabcb1fd34e7fbe90e
lzbkl <lzbkln@mail.ru>
2023-05-10 21:39:13 +0500

    edit timer ttl

--- robots/src/main/java/org/dl/app/View/CreatorTimer.java
+++ robots/src/main/java/org/dl/app/View/CreatorTimer.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CreatorTimer extends JFrame {
-    //private int TTL = 0; // in seconds
     private static final int DELAY_MS = 50; // частота обновления времени в миллисекундах
 
     private int lifeTimeSeconds = 140; // текущее время жизни робота в секундах
@@ -24,9 +23,8 @@ public class CreatorTimer extends JFrame {
 
         // создаем таймер для обновления времени
         Timer timer = new Timer(DELAY_MS, e -> {
-            //lifeTimeSeconds--;
             progressBar.setValue(lifeTimeSeconds);
-            if (lifeTimeSeconds <= 10) {
+            if (lifeTimeSeconds <= 0) {
                 ((Timer)e.getSource()).stop();// останавливаем таймер при истечении времени жизни робота
             }
         });
